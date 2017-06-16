6a89f4791b0b657625034fff61e80e9ef5d4337c
Chen Yong <290363831@qq.com>
2017-06-16 11:40:43 +0800

    Initial
    Signed-off-by: Chen Yong <290363831@qq.com>

---
+++ MyLibDemo/MyLibDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyLibDemo
+//
+//  Created by ucmed on 2017/6/16.
+//  Copyright © 2017年 ucmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MyLibDemo/MyLibDemoTests/MyLibDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  MyLibDemoTests.m
+//  MyLibDemoTests
+//
+//  Created by ucmed on 2017/6/16.
+//  Copyright © 2017年 ucmed. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyLibDemoTests : XCTestCase
+
+@end
+
+@implementation MyLibDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
